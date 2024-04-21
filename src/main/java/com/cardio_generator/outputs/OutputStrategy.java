0ce7f0f6d5ac094e2b7fc1855489d83e0a332386
ariannys <a.cermeno@student.maastrichtuniversity.nl>
2024-04-21 19:16:19 +0200

    output strategy javadoc comments

--- src/main/java/com/cardio_generator/outputs/OutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/OutputStrategy.java
@@ -1,5 +1,15 @@
 package com.cardio_generator.outputs;
 
+/**
+ * Interface to create an Output strategy that displays the data generated of the different data generators of the simulation.
+ */
 public interface OutputStrategy {
+    /**
+     * Outputs data for a patient using the Output Strategy created
+     * @param patientId The identification number for a patient, this data is generated in PatientDataGenerator
+     * @param timestamp Time stamp of the data
+     * @param label Data label of the generator
+     * @param data The data produced by the generator
+     */
     void output(int patientId, long timestamp, String label, String data);
 }
