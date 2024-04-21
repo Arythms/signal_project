d4e002144cf903b93fd3517725eabc7bc1007882
ariannys <a.cermeno@student.maastrichtuniversity.nl>
2024-04-21 20:34:16 +0200

    Alert Gnerator JavaDoc

--- src/main/java/com/cardio_generator/generators/AlertGenerator.java
+++ src/main/java/com/cardio_generator/generators/AlertGenerator.java
@@ -4,6 +4,11 @@ import java.util.Random;
 
 import com.cardio_generator.outputs.OutputStrategy;
 
+/**
+ * This class generates alerts for patients based on random conditions that can be resolved on certain conditions.
+ * The main purpose of the class is to simulate patients in a health monitoring system.
+ * It does so by implementing the Patient Data Generator interface.
+ */
 public class AlertGenerator implements PatientDataGenerator {
 
     // Renamed using camelCase
@@ -11,12 +16,24 @@ public class AlertGenerator implements PatientDataGenerator {
     // Renamed variable to camelCase and added Javadoc comment
     private boolean[] alertStates; // false = resolved, true = pressed
 
-    // Added Javadoc comment, renamed parameter to camelCase, and used final keyword
+    /**
+     * Constructs an object of the Alert generator with the amount of patients.
+     * @param patientCount The number of patients for which data will be generated
+     */
+    // Added: renamed parameter to camelCase, and used final keyword
     public AlertGenerator(final int patientCount) {
         alertStates = new boolean[patientCount + 1];
     }
 
-    // Added Javadoc comment, renamed parameter to camelCase, and used @Override
+    // Added: renamed parameter to camelCase, and used @Override
+
+    /**
+     * Generates alerts for a patient. It does so by randomly triggering the alerts and if they are withing the preffered conditions the alert is resolved.
+     * @param patientId the patient id number that was generated
+     * @param outputStrategy The strategy used to output the specific type of data
+     * @throws NullPointerException   if outputStrategy is null.
+     * @throws ArrayIndexOutOfBoundsException if patientId is out of bounds of the alertStates array.
+     */
     @Override
     public void generate(final int patientId, final OutputStrategy outputStrategy) {
         try {
